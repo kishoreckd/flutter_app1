db55ea47937c79f4ebf7dcc4720932bafafa25ce
Kishore Kumar <kishorekumarcdckap@gmail.com>
2023-09-26 11:11:59 +0530

    font added here

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,23 @@ void main() => runApp(MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: const Text('My first app'),
+        backgroundColor: Colors.red[600],
       ),
       body: const Center(
-        child: Text('Vadakkan')), 
+        child: Text('Vadakkan',
+        style: TextStyle(
+          fontSize: 26,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color:Color.fromARGB(255, 158, 156, 156),
+          fontFamily: 'montserrat'
+        ),
+        )
+
+        ), 
       floatingActionButton:  FloatingActionButton(
         onPressed: () {  },
         child: const Text('click'),
       ),
       
-          )));
+     )));
