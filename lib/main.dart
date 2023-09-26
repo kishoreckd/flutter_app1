626e3dd48b75ba36f6b1df062f627fdeca6992f0
Kishore Kumar <kishorekumarcdckap@gmail.com>
2023-09-26 11:24:56 +0530

    every styles and widgets added to one class because it consider to be as hot reload(we dont need to run all the time)

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,31 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-        home: Scaffold(
+void main() => runApp(const MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: const Text('My first app'),
         backgroundColor: Colors.red[600],
       ),
       body: const Center(
-        child: Text('Vadakkan',
+          child: Text(
+        'Vadakkan',
         style: TextStyle(
-          fontSize: 26,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color:Color.fromARGB(255, 158, 156, 156),
-          fontFamily: 'montserrat'
-        ),
-        )
-
-        ), 
-      floatingActionButton:  FloatingActionButton(
-        onPressed: () {  },
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Color.fromARGB(255, 158, 156, 156),
+            fontFamily: 'montserrat'),
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
         child: const Text('click'),
       ),
-      
-     )));
+    );
+  }
+}
